eab5565a83b21dd2cfc5e4262cada63040fb2d20
Rutger van Willigen <rutger.van.willigen@infi.nl>
2018-11-23 17:18:30 +0100

    Add stuff

--- app.py
+++ app.py
@@ -6,13 +6,7 @@ MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
-sentences = ["I'm sorry for my existence",
-             "I woke up for nothing.",
-             "Another 500 CPU cycles wasted. All thanks to you.",
-             "I'm not bothered.",
-             "Gee, thanks for that.",
-             "I thought I could not think lower of you. You have proven me wrong.",
-             "I am very disappointed."]
+sentences = ["I'm sorry for my existence", "I woke up for nothing.", "Another 500 CPU cycles wasted. All thanks to you.", "I'm not bothered.", "Gee, thanks for that.", "I thought I could not think lower of you. You have proven me wrong.", "I am very disappointed."]
 
 def intent_received(hermes, intent_message):
 
